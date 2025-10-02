f71fedd09ef11c32ffd044a0f6a43fac7d71556b
dx9 <dx9@cultofintellect.com>
2025-10-03 00:16:23 +0100

    pc is now passed in every instruction creation

--- src/decoder/decoder.cpp
+++ src/decoder/decoder.cpp
@@ -4,19 +4,19 @@ decoder::decoder(){};
 
 std::expected<std::unique_ptr<instruction>, std::string>
 decoder::to_instr(std::uint32_t instr, std::uint32_t *regs,
-                  std::vector<std::uint8_t> *memory) {
+                  std::vector<std::uint8_t> *memory, std::uint32_t *pc) {
   // check every opcode, there are different opcodes for the same instruction
   // type so we'll have some fallthroughs too.
   std::uint32_t opcode = instr & 0x7f;
 
   switch (opcode) {
   case 0x33:
-    return std::make_unique<instruction_r>(instr, regs, memory);
+    return std::make_unique<instruction_r>(instr, regs, memory, pc);
   case 0x03:
   case 0x13:
-    return std::make_unique<instruction_i>(instr, regs, memory);
+    return std::make_unique<instruction_i>(instr, regs, memory, pc);
   case 0x23:
-    return std::make_unique<instruction_s>(instr, regs, memory);
+    return std::make_unique<instruction_s>(instr, regs, memory, pc);
   }
 
   return std::unexpected("invalid opcode");
