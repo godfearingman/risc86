d8bb041f47a058bc1fe07ca228341e89ff9911e6
dx9 <dx9@cultofintellect.com>
2025-10-03 00:42:30 +0100

    added include for b type instructions

--- src/decoder/decoder.hpp
+++ src/decoder/decoder.hpp
@@ -7,6 +7,7 @@
 #include "../instruction/i_type/i_type.hpp"
 #include "../instruction/r_type/r_type.hpp"
 #include "../instruction/s_type/s_type.hpp"
+#include "../instruction/b_type/b_type.hpp"
 
 class decoder {
 private:
