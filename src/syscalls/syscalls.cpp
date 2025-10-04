30817868166bdeed65172de3600ea7a496823a4e
dx9 <dx9@cultofintellect.com>
2025-10-04 23:47:16 +0100

    registered the few syscalls we have so far; write, read & exit

--- src/syscalls/syscalls.cpp
+++ src/syscalls/syscalls.cpp
@@ -1,4 +1,6 @@
 #include "syscalls.hpp"
+#include "impl/io.hpp"
+#include "impl/process.hpp"
 
 void syscall_handler::register_syscall(std::uint32_t id, syscall_fn fn) {
   map[id] = fn;
@@ -17,7 +19,12 @@ void syscall_handler::find_and_invoke(std::uint32_t id, std::uint32_t *regs,
 }
 
 void syscall_handler::setup() {
-  // register all syscalls here
+  // io syscalls
+  register_syscall(64, syscalls::syscall_write);
+  register_syscall(63, syscalls::syscall_read);
+
+  // process syscalls
+  register_syscall(93, syscalls::syscall_exit);
 }
 
-syscall_handler::syscall_handler() {}
+syscall_handler::syscall_handler() { setup(); }
