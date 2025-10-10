31d9a6600bcf5c12d9d70f077cce2002f168ce17
dx9 <dx9@cultofintellect.com>
2025-10-10 03:49:07 +0100

    added close, fstat

--- src/syscalls/impl/io.hpp
+++ src/syscalls/impl/io.hpp
@@ -6,6 +6,17 @@
 #include <vector>
 
 namespace syscalls {
+inline void syscall_close(std::uint32_t *regs, std::vector<std::uint8_t> *mem) {
+  std::int32_t fd = regs[10];
+
+  if (fd >= 0 && fd <= 2) {
+    regs[10] = 0;
+    return;
+  }
+
+  std::int32_t ret = _close(fd);
+  regs[10] = ret;
+}
 inline void syscall_write(std::uint32_t *regs, std::vector<std::uint8_t> *mem) {
   std::int32_t fd = regs[10];
   std::uint32_t buff = regs[11];
@@ -27,5 +38,24 @@ inline void syscall_read(std::uint32_t *regs, std::vector<std::uint8_t> *mem) {
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
   }
   regs[10] = read;
+}
+inline void syscall_fstat(std::uint32_t *regs, std::vector<std::uint8_t> *mem) {
+  std::int32_t fd = regs[10];
+  std::uint32_t statbuf = regs[11];
+
+  if (fd >= 0 && fd <= 2) {
+    std::uint32_t s_ifchr = 0x2000;
+
+    // st_mode is 8 bytes into the statbuf struct
+    (*mem)[statbuf + 8] = (s_ifchr & 0xff);
+    (*mem)[statbuf + 9] = (s_ifchr >> 8) & 0xff;
+    (*mem)[statbuf + 10] = (s_ifchr >> 16) & 0xff;
+    (*mem)[statbuf + 11] = (s_ifchr >> 24) & 0xff;
+
+    regs[10] = 0;
+
+  } else {
+    regs[10] = -9; // -EBADF
+  }
 }
 } // namespace syscalls
