decbc85127a38ad022d55d91f1bee32b1e5abd76
dx9 <dx9@cultofintellect.com>
2025-10-11 01:16:03 +0100

    base

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,25 @@
 #include "handler/handler.hpp"
 
 int main() {
-  // TODO: ADD JALR WHEN U WAKE UP
+  auto hnd = riscv_handler();
+
+  //hnd.enable_debug(true);
+  //hnd.enable_trace(true);
+
+
+  hnd.load_program("C:\\Users\\sohai\\Downloads\\riscv_tests\\test\\donut\\donut.elf");
+
+  while (true) {
+    auto result = hnd.step_prog();
+    if (!result.has_value()) {
+      std::puts(result.error().c_str());
+      break;
+    }
+  }
+
+  std::puts("");
+
   std::getchar();
+
   return 0;
 }
