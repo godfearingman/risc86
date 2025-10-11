5a8175d5c2fd7cb7fad908e24b7d9304e09b7b92
dx9 <dx9@cultofintellect.com>
2025-10-11 01:09:13 +0100

    added fregs to param list

--- src/decoder/decoder.hpp
+++ src/decoder/decoder.hpp
@@ -4,12 +4,12 @@
 #include <memory>
 #include <string>
 
+#include "../instruction/b_type/b_type.hpp"
 #include "../instruction/i_type/i_type.hpp"
+#include "../instruction/j_type/j_type.hpp"
 #include "../instruction/r_type/r_type.hpp"
 #include "../instruction/s_type/s_type.hpp"
-#include "../instruction/b_type/b_type.hpp"
 #include "../instruction/u_type/u_type.hpp"
-#include "../instruction/j_type/j_type.hpp"
 
 class decoder {
 private:
@@ -19,5 +19,6 @@ public:
   decoder();
   std::expected<std::unique_ptr<instruction>, std::string>
   to_instr(std::uint32_t instr, std::uint32_t *regs,
-           std::vector<std::uint8_t> *memory, std::uint32_t *pc);
+           std::vector<std::uint8_t> *memory, std::uint32_t *pc,
+           std::uint64_t *fregs);
 };
